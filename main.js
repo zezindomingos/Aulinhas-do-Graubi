3f81e26f4a383def3e91d864c2a63f07f7f23d60
José Matheus <99216440+zezindomingos@users.noreply.github.com>
2022-02-07 19:08:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const links = {
+  github: 'zezindomingos',
+  instagram: 'joo_math',
+  facebook: 'josematheus.domingos',
+  twitter: 'jose_math1'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${links[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${links.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
